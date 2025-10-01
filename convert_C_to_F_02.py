1e14213d7dd5771e037513d897e1d70b07d172c2
NdEllis89 <nellis8@fingerlakes.edu>
2025-10-01 22:57:33 +0000

    spaces

--- convert_C_to_F_02.py
+++ convert_C_to_F_02.py
@@ -21,8 +21,8 @@
 print('')
 print('===== Temperature Converter =====')
 print('')
-print(' 1. Convert from Celsius to Fahrenheit ')
-print(' 2. Convert from Fahrenheit to Celsius ')
+print('  1. Convert from Celsius to Fahrenheit ')
+print('  2. Convert from Fahrenheit to Celsius ')
 print('')
 
 choice = int(input('Please choose from the above menu: '))
